e23fe60930c170def42eed1ca604449c7401d2dd
RandallBPotter15 <rantheman@gmail.com>
2016-07-13 14:13:20 -0400

    Forgot Content-Type

--- src/server/server.js
+++ src/server/server.js
@@ -27,7 +27,7 @@ app.use(require('morgan')("combined", { "stream": logger.stream }));
 
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
     res.header("Access-Control-Allow-Methods", "GET, POST","PUT");
     next();
 });
