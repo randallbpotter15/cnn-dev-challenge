f1f1610e0db1227add7e4adb37b232522409d539
RandallBPotter15 <rantheman@gmail.com>
2016-07-13 16:22:56 -0400

    search field

--- src/client/app/controllers/appController.js
+++ src/client/app/controllers/appController.js
@@ -7,23 +7,14 @@ angular.module('appController', [])
         //console.log(Tweets.getTweets());
 
 
-        $http.get("http://localhost:3001/api/tweets/")
-        .then(function mySuccess(response) {
-            console.log(response.data);
-            $scope.tweets = response.data.statuses;
-        }, function myError(response) {
-            console.log(response.statusText);
-        });
-
         $scope.searchTwitter = function() {
             var searchURL = "http://localhost:3001/api/tweets/" + $scope.searchQuery;
             console.log(searchURL);
             $http.get(searchURL)
                 .then(function mySuccess(response) {
-                    console.log(response.data);
                     $scope.tweets = response.data.statuses;
                 }, function myError(response) {
-                    console.log(response.statusText);
+                    throw new Error("Please try again later...");
                 });
 
         }
